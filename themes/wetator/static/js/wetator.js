6a54813b0a7af12627dd27cd7cecafbd5b1c6522
Ronald Brill <rbri@rbri.de>
2026-04-03 14:16:07 +0200

    try to make it responsive

--- themes/wetator/static/js/wetator.js
+++ themes/wetator/static/js/wetator.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+  // Mobile navigation toggle
+  $('#nav_toggle').on('click', function() {
+    var nav = $('#header_nav');
+    var expanded = nav.hasClass('open');
+    nav.toggleClass('open');
+    $(this).attr('aria-expanded', !expanded);
+  });
+
+  // Close nav when a link is clicked on mobile
+  $('#header_nav a').on('click', function() {
+    if ($(window).width() <= 600) {
+      $('#header_nav').removeClass('open');
+      $('#nav_toggle').attr('aria-expanded', false);
+    }
+  });
+
+  // Toggle panels
   $(".togglePanel_content").hide(); 
   $(".togglePanel").click(function(){
     if ($(this).hasClass("togglePanel_open")) {
